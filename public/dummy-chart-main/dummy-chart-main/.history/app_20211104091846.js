3bc69339bedd0f641d19316a5aefd4bff7682f73
andrealeporialeide <a.lepori@aleide.it>
2021-11-04 14:18:23 +0100

    1418
    1418

---
+++ public/dummy-chart-main/dummy-chart-main/.history/app_20211104091846.js
@@ -0,0 +1,57 @@
+var employeeLabel = [], employeeSalaryData = [], employeeAgeData = []
+
+async function dummyChart() {
+  await getDummyData()
+
+const ctx = document.getElementById('myChart').getContext('2d');
+
+const chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: employeeLabel,
+        datasets: [{
+            label: 'Employee Salary',
+            backgroundColor: '',
+            borderColor: '#0A2558',
+            data: employeeSalaryData
+        },
+        {
+          label: 'Employee Age',
+          backgroundColor: '',
+          borderColor: '#0A2558',
+          data: employeeAgeData
+      }
+      ]
+    },
+
+    // Configuration options go here
+    options: {
+      tooltips: {
+        mode: 'index'
+      }
+    }
+})}
+
+dummyChart()
+
+
+//Fetch Data from API
+
+async function getDummyData() {
+  const apiUrl = "https://andrealeporialeide.github.io/invest.io/public/dummy-chart-main/restapi.json";
+
+  const response = await fetch(apiUrl)
+  const barChatData = await response.json()
+  
+  const salary = barChatData.data.map((x) => x.employee_salary)
+  console.log(salary)
+  const age = barChatData.data.map((x) => x.employee_age)
+  const name = barChatData.data.map((x) => x.employee_date)
+
+ employeeSalaryData = salary
+ employeeAgeData = age
+ employeeLabel = name
+}
---
+++ public/dummy-chart-main/dummy-chart-main/app.js
@@ -0,0 +1,59 @@
+var employeeLabel = [], date = [],employee_date = []
+
+async function dummyChart() {
+  await getDummyData()
+
+const ctx = document.getElementById('myChart').getContext('2d');
+
+//Fetch Data from API
+
+async function getDummyData() {
+  const apiUrl = "https://andrealeporialeide.github.io/invest.io/public/dummy-chart-main/restapi.json";
+
+  const response = await fetch(apiUrl)
+  const barChatData = await response.json()
+  
+  const salary = barChatData.data.map((x) => x.employee_salary)
+  console.log(salary)
+  const age = barChatData.data.map((x) => x.employeeLabel)
+  const date = barChatData.data.map((x) => x.date)
+
+ employee_date = salary;
+ employeeLabel= date;
+
+}
+const chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+      labels: employee_date, employeeLabel,
+      
+        datasets: [{
+            label: 'Employee Salary',
+            backgroundColor: '',
+            borderColor: '#0A2558',
+            data: employee_date
+        },
+        {
+          label: 'Employee Date',
+          backgroundColor: '',
+          borderColor: '#0A2558',
+          data: employeeLabel,
+      },
+
+      ]
+    },
+
+    // Configuration options go here
+    options: {
+      tooltips: {
+        mode: 'index'
+      }
+    }
+})}
+
+dummyChart()
+
+

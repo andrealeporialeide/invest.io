6c37796ad776f641c7cc3fed95b82b7fb3c044d1
andrealeporialeide <93589326+andrealeporialeide@users.noreply.github.com>
2021-11-04 14:21:49 +0100

    Update app.js

--- dummy-chart-main/app.js
+++ dummy-chart-main/app.js
@@ -1,29 +1,48 @@
-var employeeLabel = [], employeeSalaryData = [], employeeAgeData = []
+var employeeLabel = [], date = [],employee_date = [], employee_name=[]
 
 async function dummyChart() {
   await getDummyData()
 
 const ctx = document.getElementById('myChart').getContext('2d');
 
+//Fetch Data from API
+
+async function getDummyData() {
+  const apiUrl = "https://andrealeporialeide.github.io/invest.io/public/dummy-chart-main/restapi.json";
+
+  const response = await fetch(apiUrl)
+  const barChatData = await response.json()
+  
+  const salary = barChatData.data.map((x) => x.employee_salary)
+  console.log(salary)
+  const age = barChatData.data.map((x) => x.employeeLabel)
+  const date = barChatData.data.map((x) => x.date)
+
+ employee_date = salary;
+ employeeLabel= date;
+
+}
 const chart = new Chart(ctx, {
     // The type of chart we want to create
     type: 'line',
 
     // The data for our dataset
     data: {
-        labels: employeeLabel,
+      labels: employee_date, employee_name,
+      
         datasets: [{
             label: 'Employee Salary',
             backgroundColor: '',
             borderColor: '#0A2558',
-            data: employeeSalaryData
+            data: employee_date
         },
         {
-          label: 'Employee Age',
+          label: 'Employee Date',
           backgroundColor: '',
           borderColor: '#0A2558',
-          data: employeeAgeData
-      }
+          data: employee_name,
+      },
+
       ]
     },
 
@@ -36,22 +55,3 @@ const chart = new Chart(ctx, {
 })}
 
 dummyChart()
-
-
-//Fetch Data from API
-
-async function getDummyData() {
-  const apiUrl = "https://andrealeporialeide.github.io/invest.io/public/dummy-chart-main/restapi.json";
-
-  const response = await fetch(apiUrl)
-  const barChatData = await response.json()
-  
-  const salary = barChatData.data.map((x) => x.employee_salary)
-  console.log(salary)
-  const age = barChatData.data.map((x) => x.employee_age)
-  const name = barChatData.data.map((x) => x.employee_name)
-
- employeeSalaryData = salary
- employeeAgeData = age
- employeeLabel = name
-}
